896e8549412a4445d1e77f50f99ce067e874f0e7
lwauke <lwauke@hotmail.com>
2018-11-26 05:08:37 -0200

    using font-awesome icons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   listItems = [
-    {text: 'Painel de Gestão', icon: 'icon'},
-    {text: 'Conta Digital', icon: 'icon'},
-    {text: 'Ant. de Recebíveis', icon: 'icon'},
+    {text: 'Painel de Gestão', icon: 'globe'},
+    {text: 'Conta Digital', icon: 'at'},
+    {text: 'Ant. de Recebíveis', icon: 'chart-bar'},
   ]
 
   responsiveMenuShowing: false;
